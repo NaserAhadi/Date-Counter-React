d34eddbd60f3aa039e1b8e7c8c638e70257071dd
Naser Ahadi <n.ahadi@vandar.io>
2024-12-31 12:18:26 +0330

    add slider and input to project

--- src/App.js
+++ src/App.js
@@ -8,13 +8,6 @@ export default function App() {
     const date = new Date()
     date.setDate(date.getDate() + count)
 
-    function handlePreviousStep(){
-        if(step>1) setStep(s => s -1);
-    }
-    function handleNextStep(){
-        setStep(s => s +1);
-    }
-
     function handlePreviousCount(){
         setCount(c => c-step)
     }
@@ -22,23 +15,28 @@ export default function App() {
         setCount(c => c+step);
     }
 
+    function handleReset(){
+        setStep(1)
+        setCount(0);
+    }
+
   return (
       <div className="App">
-          <div className="buttons-container">
-              <button onClick={handlePreviousStep}>-</button>
-              <span>Step: {step}</span>
-              <button onClick={handleNextStep}>+</button>
+          <div>
+              <input type="range" min="0" max="10" value={step} onChange={(e) => setStep(Number(e.target.value))}/>
+              <span>{step}</span>
           </div>
           <div className="buttons-container">
               <button onClick={handlePreviousCount}>-</button>
-              <span>Count: {count}</span>
+              <input type="text" value={count} onInput={e => setCount(Number(e.target.value)?Number(e.target.value):0)}/>
               <button onClick={handleNextCount}>+</button>
           </div>
           <p>
-              {count===0 && <span>Today is <b className="date">{date.toDateString()}</b></span>}
+          {count===0 && <span>Today is <b className="date">{date.toDateString()}</b></span>}
               {count>0 && <span><b>{count}</b> days from Today is <b className="date">{date.toDateString()}</b> </span>}
               {count<0 && <span><b>{Math.abs(count)}</b> days ago was <b className="date">{date.toDateString()}</b> </span>}
           </p>
+          {(step !== 1 || count !== 0) && <button onClick={handleReset}>Reset</button> }
       </div>
   );
 }
